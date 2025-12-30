e96994a038e0b6bbf8aff3001d909f926d646335
박주영 <ju0gkorea@gmail.com>
2025-12-30 09:48:17 +0900

    Initial commit: ROS2 package setup with Git LFS for YOLO models

---
+++ emergency_camera/emergency_camera/yolo_detector.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from emergency_camera.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ emergency_camera/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'emergency_camera'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='juyeong',
+    maintainer_email='ju0gkorea@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'yolo_detector = emergency_camera.yolo_detector:main'
+        ],
+    },
+)
